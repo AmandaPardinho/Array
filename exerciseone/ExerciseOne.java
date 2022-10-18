bd4a8fef0f2dcfeb279a0170394bb5f1873e9889
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-17 21:08:15 -0300

    Starting to think about the solution

---
+++ exerciseone/ExerciseOne.java
@@ -0,0 +1,27 @@
+package exerciseone;
+
+public class ExerciseOne {
+
+    public static void main(String[] args) {
+        /*Escreva um programa Java GUI ou console que declara, constrói e inicializa um vetor de 10 inteiros.
+        Em seguida peça para que o usuário informe um valor a ser pesquisado. Faça uma varredura no vetor e
+        informe quantas vezes o valor pesquisado é encontrado:
+
+        int valores[] = {4, 21, 9, 8, 12, 21, 4, 4, 1, 10};
+         */
+        int number;
+        int values[];
+
+        while(number < 22 && number > 0){
+
+        }
+        values.add(number);
+                //= {4, 21, 9, 8, 12, 21, 4, 4, 1, 10};
+
+        for(int i = 0; i < 10; i++){
+            if(int values [i] == int values [i + 1]){
+
+            }
+        }
+    }
+}

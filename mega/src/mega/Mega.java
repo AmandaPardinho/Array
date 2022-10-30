f3a31e1eace82ef0b21e5c4ef71b5e50ac79ffbd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-29 22:46:35 -0300

    Attempting to fix the error

--- mega/src/mega/Mega.java
+++ mega/src/mega/Mega.java
@@ -1,25 +1,26 @@
 package mega;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Mega {
     public static void main(String[] args) {
 
-        double ale = Math.random();
-        Integer numeroSorteado = (int) (1 + ale * (60 - 1));
+        Integer ale = Math.random();
+        Integer numeroSorteado = (Integer) (1 + ale * (60 - 1));
         System.out.println(numeroSorteado);
         int i;
 
-        double[] mega = new double[5];
+        Integer[] mega = new Integer[5];
         //int size = mega.size();
 
 
-        for(i = 0; i <= mega.length; i++){
+        for(i = 0; i <= 5; i++){
             mega[i] = numeroSorteado;
             i++;
-            System.out.println(mega);
+            System.out.println(mega[i]);
         }
     }
 }

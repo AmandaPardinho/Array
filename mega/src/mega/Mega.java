094f3a03c6c04fb40d8ffb590bed94648c16f493
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-20 22:27:57 -0300

    New attempt at array resolution

--- mega/src/mega/Mega.java
+++ mega/src/mega/Mega.java
@@ -1,24 +1,25 @@
 package mega;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Scanner;
 
 public class Mega {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        
         float ale = (float) Math.random();
         float numeroSorteado;
-        numeroSorteado = (float) (1 + (ale * (60 - 1)));
-        System.out.println(numeroSorteado);
+        numeroSorteado = 1 + (ale * (60 - 1));
+
         int i;
 
         Integer[] mega = new Integer[5];
-        //int size = mega.size();
+        float [] sorteio = new float[60];
 
 
         for(i = 0; i <= 5; i++){
+            for(j = 1; j <= 60; j++){
+
+            }
             mega[i] = Float.(numeroSorteado);
             i++;
             System.out.println(mega[i]);

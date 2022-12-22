1de3f0a42f62bbf9a7c6919707e997ca4fa86553
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-22 20:25:31 -0300

    Changes in code

--- mega/src/mega/Mega.java
+++ mega/src/mega/Mega.java
@@ -1,18 +1,17 @@
 package mega;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Mega {
     public static void main(String[] args) {
         Random random = new Random();
+        Set<Integer> mega = new HashSet<Integer>();
 
-        int number[] = new int[6];
-
-        for(int i = 0; i < number.length; i++){
-            number[i] = random.nextInt(60);
-            Arrays.stream(number).anyMatch(j -> j != random.nextInt(60));
-            System.out.println(number[i]);
+        while(mega.size() < 6){
+            mega.add(random.nextInt(60) + 1);
         }
+        System.out.println(mega);
     }
 }
